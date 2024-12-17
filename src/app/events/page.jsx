eeea3349e14cfa95075dfd73addb4260d2437027
Vivek Kumar <vivekyadavnitt@gmail.com>
2024-12-17 10:21:43 +0530

    component skelton added

---
+++ src/app/events/page.jsx
@@ -0,0 +1,6 @@
+const page = () => {
+  return (
+    <div>Events Page</div>
+  )
+}
+export default page
---
+++ src/app/team/page.jsx
@@ -0,0 +1,8 @@
+
+const page = () => {
+  return (
+    <div>Team Page</div>
+  )
+}
+
+export default page
