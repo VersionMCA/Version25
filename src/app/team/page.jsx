c4410fe6f36d94ae9be583be9ca09d03787d165d
Chirag Tomar <chiragtomar251@gmail.com>
2024-12-18 20:21:47 +0530

    Props has been settled

--- src/app/team/page.jsx
+++ src/app/team/page.jsx
@@ -1,18 +1,20 @@
 import ProfileCard from "../../components/teams/ProfileCard"
+import Teams from "../../helper/teams"
 
 const page = () => {
+  const [EEC_Members,PPC_Members]=Teams;
   return (
     <div className = "flex justify-center items-center p-4 flex-col bg-[#1E1E1E] ">
       <div className = "text-center font-extrabold font-mono ">Team Page</div>
       <div className = "flex justify-center items-center flex-wrap gap-8 p-10">
-        <ProfileCard />
-        <ProfileCard />
-        <ProfileCard />
-        <ProfileCard />
-        <ProfileCard />
-        <ProfileCard />
-
-
+        {EEC_Members.map((profile,index)=>(
+        <ProfileCard   
+        key={index}
+        name={profile.name}
+        image={profile.image}
+        socialLinks={profile.socialLinks}
+        />
+        ))}
       </div>
     </div>
   )
---
+++ src/helper/teams.js
@@ -0,0 +1,57 @@
+const EEC_Members=[ {
+    name: "Chirag Tomar",
+    image: '../assets/EEC_Images/Chirag.jpg',
+    socialLinks: {
+      github: "https://github.com/andresberlin",
+      twitter: "https://twitter.com/andresberlin",
+      instagram: "https://instagram.com/andresberlin",
+    }},
+{
+    name: "Vivek Kumar",
+    image: '../assets/EEC_Images/Chirag.jpg',
+    socialLinks: {
+        github: "https://github.com/andresberlin",
+        twitter: "https://twitter.com/andresberlin",
+        instagram: "https://instagram.com/andresberlin",
+      }
+},
+{
+    name: "Vivek Kumar",
+    image: '../assets/EEC_Images/Chirag.jpg',
+    socialLinks: {
+        github: "https://github.com/andresberlin",
+        twitter: "https://twitter.com/andresberlin",
+        instagram: "https://instagram.com/andresberlin",
+      }
+},
+{
+    name: "Vivek Kumar",
+    image: '../assets/EEC_Images/Chirag.jpg',
+    socialLinks: {
+        github: "https://github.com/andresberlin",
+        twitter: "https://twitter.com/andresberlin",
+        instagram: "https://instagram.com/andresberlin",
+      }
+},
+{
+    name: "Vivek Kumar",
+    image: '../assets/EEC_Images/Chirag.jpg',
+    socialLinks: {
+        github: "https://github.com/andresberlin",
+        twitter: "https://twitter.com/andresberlin",
+        instagram: "https://instagram.com/andresberlin",
+      }
+},
+{
+    name: "Vivek Kumar",
+    image: '../assets/EEC_Images/Chirag.jpg',
+    socialLinks: {
+        github: "https://github.com/andresberlin",
+        twitter: "https://twitter.com/andresberlin",
+        instagram: "https://instagram.com/andresberlin",
+      }
+}];
+const PPC_members=[];
+const Teams=[EEC_Members,PPC_members];
+
+export default Teams;
