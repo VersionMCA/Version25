74ba54a097cd2f036a19ec25f93886cd6e8f98ae
priyanshuwu <priyanshumiglani8@gmail.com>
2024-12-18 18:35:06 +0530

    ProfileCard component added for further changes

--- src/app/team/page.jsx
+++ src/app/team/page.jsx
@@ -1,7 +1,20 @@
+import ProfileCard from "../../components/teams/ProfileCard"
 
 const page = () => {
   return (
-    <div>Team Page</div>
+    <div className = "flex justify-center items-center p-4 flex-col bg-[#1E1E1E] ">
+      <div className = "text-center font-extrabold font-mono ">Team Page</div>
+      <div className = "flex justify-center items-center flex-wrap gap-8 p-10">
+        <ProfileCard />
+        <ProfileCard />
+        <ProfileCard />
+        <ProfileCard />
+        <ProfileCard />
+        <ProfileCard />
+
+
+      </div>
+    </div>
   )
 }
 
--- src/components/teams/ProfileCard.jsx
+++ src/components/teams/ProfileCard.jsx
@@ -1,6 +1,13 @@
 const ProfileCard = () => {
   return (
-    <div>ProfileCard</div>
+    <div className = "rounded-lg aspect-[1/1] overflow-hidden max-w-xs  lg:max-w-sm flex justify-center  items-center flex-col relative select-none bg-[#1E1E1E] border-double hover:border-l-8 hover:border-t-8 hover:border-l-[#b7f625] hover:border-t-[#b7f625] transition-all ease-in-out">
+      <img src="https://i.pinimg.com/736x/3d/3b/f1/3d3bf1f13bc997907db8aa68e4ab5153.jpg" alt="" srcSet="https://i.pinimg.com/736x/3d/3b/f1/3d3bf1f13bc997907db8aa68e4ab5153.jpg" className = "w-full h-full object-cover"/>
+      <div className="bg-[#A5F524] p-2 sm:p-3 rounded-2xl border-2 border-black absolute bottom-3 sm:bottom-6 font-mono text-black flex justify-center flex-col items-center">
+        <p className = "font-bold text-md sm:text-xl">Tyler Durden</p>
+        <p className = "text-sm sm:text-md">The Creator</p>
+      </div>
+    </div>
+ 
   )
 }
 
