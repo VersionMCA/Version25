4c6828b9670d5a4a8a38206a257804dd06b668d7
Chirag Tomar <chiragtomar251@gmail.com>
2024-12-20 00:03:51 +0530

    configs files updated

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,13 @@
-import Map from "@/components/home/Map";
-
-
+"use client";
+import { signOut } from "next-auth/react";
+import Calendar from "../components/home/Calendar";
+import About from "../components/home/About";
 export default function Home() {
   return (
     <div>
-      <Map />
+      <Calendar />
+      <About />
+      <button onClick={() => signOut()}> signOut</button>
     </div>
   );
 }
