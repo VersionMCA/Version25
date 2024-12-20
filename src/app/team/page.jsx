7c42f19477314873f311893ad616c0e16a949b77
Chirag Tomar <chiragtomar251@gmail.com>
2024-12-20 16:05:33 +0530

    Designing done And responsive done

--- src/app/team/page.jsx
+++ src/app/team/page.jsx
@@ -8,16 +8,15 @@ const links={
 
 const page = () => {
   const {EEC_Members, PPC_Members, Faculty,CCC_Members,EEC_Heads,PRC_Members,PRC_Heads,PPC_Heads,HRC_Members,HRC_Heads,ARC_Members,ARC_Heads} = Teams;
-  console.log(PRC_Members)
   return (
-    <div className="flex justify-center items-center p-4 flex-col bg-[#1E1E1E] ">
+    <div className="flex justify-center items-center p-4 flex-col bg-[#1E1E1E] bg-gradient-to-r from-slate-600 to-gray-800 ">
       <div className="opacity-1 transform-none font-extrabold font-mono text-white">
-        <h1 className="text-center text-3xl md:text-4xl mb-10 ">
-          MEET THE <span className="text-[#A5F524]"> TEAM </span>
+        <h1 className="text-center text-3xl md:text-4xl mb-10 bg-gradient-to-r from-slate-200 to-gray-400 bg-clip-text text-transparent hover:scale-105 transition duration-500">
+          MEET THE <span className="bg-gradient-to-r from-lime-200 to-lime-400 bg-clip-text text-transparent"> TEAM </span>
         </h1>
       </div>
       <div className=" flex items-center justify-center flex-col flax-wrap">
-        <h3 className=" text-center font-bold text-[#A5F524]">
+        <h3 className=" text-center font-bold bg-gradient-to-r from-lime-200 to-lime-400 bg-clip-text text-transparent text-3xl hover:scale-105 transition duration-500">
           HEAD OF THE DEPARTMENT & STAFF ADVISOR
         </h3>
         <div className="flex justify-center items-center flex-wrap p-10 mb-10 gap-8">
@@ -33,7 +32,7 @@ const page = () => {
         </div>
       </div>
       <div className=" flex items-center justify-center flex-col flax-wrap">
-        <h3 className=" text-center font-bold text-[#A5F524]">
+        <h3 className=" text-center font-bold bg-gradient-to-r from-lime-200 to-lime-400 bg-clip-text text-transparent text-3xl hover:scale-105 transition duration-500">
           CENTRAL CORDINATION COMMITEE
         </h3>
         <div className="flex justify-center items-center flex-wrap gap-8 mb-10 p-10">
@@ -57,7 +56,7 @@ const page = () => {
         </div>
       </div>
       <div className=" flex items-center justify-center flex-col flax-wrap">
-        <h3 className=" text-center font-bold text-[#A5F524]">
+        <h3 className=" text-center font-bold bg-gradient-to-r from-lime-200 to-lime-400 bg-clip-text text-transparent text-3xl hover:scale-105 transition duration-500">
           EVENT EXECUTION COMMITEE
         </h3>
         <div className="flex justify-center items-center flex-wrap gap-8 mb-10 p-10">
@@ -92,7 +91,7 @@ const page = () => {
         </div>
       </div>
       <div className=" flex items-center justify-center flex-col flax-wrap">
-        <h3 className=" text-center font-bold text-[#A5F524]">
+        <h3 className=" text-center font-bold bg-gradient-to-r from-lime-200 to-lime-400 bg-clip-text text-transparent text-3xl hover:scale-105 transition duration-500">
           PUBLIC RELATION COMMITEE
         </h3>
         <div className="flex justify-center items-center flex-wrap gap-8 mb-10 p-10">
@@ -127,7 +126,7 @@ const page = () => {
         </div>
       </div>
       <div className=" flex items-center justify-center flex-col flax-wrap">
-        <h3 className=" text-center font-bold text-[#A5F524]">
+        <h3 className=" text-center font-bold bg-gradient-to-r from-lime-200 to-lime-400 bg-clip-text text-transparent text-3xl hover:scale-105 transition duration-500">
           ALUMNI RELATION COMMITEE
         </h3>
         <div className="flex justify-center items-center flex-wrap gap-8 mb-10 p-10">
@@ -162,7 +161,7 @@ const page = () => {
         </div>
       </div>
       <div className=" flex items-center justify-center flex-col flax-wrap">
-        <h3 className=" text-center font-bold text-[#A5F524]">
+        <h3 className=" text-center font-bold bg-gradient-to-r from-lime-200 to-lime-400 bg-clip-text text-transparent text-3xl hover:scale-105 transition duration-500">
           HOSPITALITY AND RECEPTION COMMITEE
         </h3>
         <div className="flex justify-center items-center flex-wrap gap-8 mb-10 p-10">
@@ -197,7 +196,7 @@ const page = () => {
         </div>
       </div>
       <div className=" flex items-center justify-center flex-col flax-wrap">
-        <h3 className=" text-center font-bold text-[#A5F524]">
+        <h3 className=" text-center font-bold bg-gradient-to-r from-lime-200 to-lime-400 bg-clip-text text-transparent text-3xl hover:scale-105 transition duration-500">
           PRINTING AND PUBLISHING COMMITEE
         </h3>
         <div className="flex justify-center items-center flex-wrap gap-8 mb-10 p-10">
