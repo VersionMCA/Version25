a7657e258c12da64e286b882f9701dd1695a677c
Vivek Kumar <142966563+109-Vivek@users.noreply.github.com>
2024-12-21 15:49:05 +0530

    Update tailwind.config.mjs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -15,6 +15,7 @@ export default {
           blue_light: "#326B83", // RGB: 50, 107, 131
           blue_dark: "#092C3B", // RGB: 9, 44, 59
           lime: "#C6FF00", // RGB: 198, 255, 0
+          light_lime: "#d4ff41", // RGB: 212, 255, 65
           cream: "#F4FFD8", // RGB: 244, 255, 216
           yellow: "#D7F205", // RGB: 215, 242, 5
           black: "#171616", // RGB: 23, 22, 22
