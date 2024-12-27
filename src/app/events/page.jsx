14318a23e49450ca55eff0a6549122d605035aa2
Vivek Kumar <vivekyadavnitt@gmail.com>
2024-12-27 17:32:31 +0530

    Events template done

--- src/app/events/page.jsx
+++ src/app/events/page.jsx
@@ -1,6 +1,42 @@
-const page = () => {
+"use client";
+import React from "react";
+import { useState } from "react";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import events from "./eventData";
+import EventCard from "./EventCard";
+import EventSlider from "./EventSlider";
+
+function Events() {
+  const [selectedEventIndex, setSelectedEventIndex] = useState(0);
+  const [showEvent, setShowEvent] = useState(false);
+
+  //Function to change the main content to the clicked event
+  function changeEvent(ind) {
+    setSelectedEventIndex(ind);
+  }
+
   return (
-    <div>Events Page</div>
-  )
+    <div className="h-full relative w-full mt-40">
+      {showEvent ? (
+        <EventCard
+          event={events[selectedEventIndex]}
+          setShowEvent={setShowEvent}
+        />
+      ) : (
+        <div className="h-screen overflow-hidden mt-44">
+          <EventSlider
+            selectedEventIndex={selectedEventIndex}
+            changeEvent={changeEvent}
+            event={events[selectedEventIndex]}
+            setShowEvent={setShowEvent}
+          />
+        </div>
+      )}
+    </div>
+  );
 }
-export default page
+
+export default Events;
