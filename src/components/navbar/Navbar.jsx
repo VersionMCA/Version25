3c5dc84c80764c300f0ee614e4edb7be3d3a5d74
Vivek Kumar <vivekyadavnitt@gmail.com>
2024-12-29 02:33:48 +0530

    changed navbar,added profile dropdown

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,37 +1,85 @@
-"use client";
-import NavMobile from "./NavMobile.jsx";
-import logo from "../../../public/assets/logo2.png";
-import NavDesktop from "./NavDesktop.jsx";
 import Link from "next/link";
-import Image from "next/image";
-import { useRef } from "react";
+import { signIn } from "next-auth/react";
+import { useSession } from "next-auth/react";
+import versionLogo from "../../../public/assets/logo2.png";
+import UserAccountDropDown from "./UserAccountDropDown";
+// import "./navbar.css"   working without importing
 
-export function Navbar({ bgWhite, noBgBlack, session }) {
-  const loading = false;
-  const navBar = useRef();
+import Image from "next/image";
+import { motion } from "framer-motion";
+import { Button } from "../ui/Button";
+import navLinks from "./navLinks";
+
+const Navbar = ({toggle}) => {
+  const session = useSession();
+  const user = session.data?.user;
 
   return (
-    <div
-      className={`absolute top-0 left-0 right-0 z-50 md:bg-black md:bg-opacity-5`}
-    >
-      <nav
-        className="container flex items-center justify-between p-4 max-md:p-2 max-sm:p-1 text-base md:text-lg uppercase min-w-full"
-        ref={navBar}
+    <nav className="sticky  mx-auto  wrapper  top-0 z-50 flex items-center gap-2 py-6 w-full">
+      <motion.div
+        initial={{ y: -20, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{
+          duration: 0.5,
+          ease: "easeInOut",
+          type: "spring",
+          damping: 10,
+        }}
+        className="flex w-full justify-between mx-auto bg-secondary/15 shadow-lg shadow-neutral-600/5 backdrop-blur-lg border border-primary/20 p-6 rounded-2xl"
       >
-        <Link
-          href="/"
-          className=" mx-6 max-md:mx-4 max-sm:mx-0 h-12 max-md:h-10 max-sm:h-8 w-44 relative flex justify-center items-center"
-        >
-          <Image src={logo} alt="logo" fill objectFit="contain" />
+        <Link href={"/"} className="flex items-center cursor-pointer">
+          <Image src={versionLogo} alt="Logo" width={230} height={600} />
         </Link>
+        <div className="flex justify-center items-center gap-2">
+          {navLinks.map(({ name, link }) => {
+            return (
+              <li
+                key={name}
+                className="mx-4 pixie md:text-2xl xl:mx-8  hidden md:block"
+              >
+                <Link href={link}>
+                  <p className="transition-all codedText tempp   ease-in-out">
+                    {name}
+                  </p>
+                </Link>
+              </li>
+            );
+          })}
+          {!user ? (
+            <Button
+              size={"lg"}
+              onClick={async () => {
+                await signIn();
+              }}
+            >
+              Login
+            </Button>
+          ) : (
+            ""
+          )}
+          <UserAccountDropDown />
 
-        {!loading && (
-          <>
-            <NavMobile navBar={navBar} session={session} />
-            <NavDesktop bgWhite={bgWhite} session={session} />
-          </>
-        )}
-      </nav>
-    </div>
+          {/* Hamburger */}
+          <li className="flex justify-end border-white text-white items-center basis-full">
+            <button type="button" className="md:hidden " onClick={toggle}>
+              <svg
+                // className="invert"
+                xmlns="http://www.w3.org/2000/svg"
+                width="40"
+                height="40"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="#fff"
+                  d="M3 6h18v2H3V6m0 5h18v2H3v-2m0 5h18v2H3v-2Z"
+                />
+              </svg>
+            </button>
+          </li>
+        </div>
+      </motion.div>
+    </nav>
   );
-}
+};
+
+export default Navbar;
