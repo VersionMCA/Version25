1071ba396c2597ca47cb7ca34c27d5560baeb63e
Vivek Kumar <142966563+109-Vivek@users.noreply.github.com>
2024-12-28 11:19:52 +0530

    Feat/events (#26)
    * added events Registeration Endpoints
    
    
    Co-authored-by: Nikhil Chhipa <chhipanikhil9@gmail.com>

---
+++ src/app/api/events/fetchRegisteredEvents/route.js
@@ -0,0 +1,31 @@
+import { getServerSession } from 'next-auth';
+import { prisma } from '@/db/index.mjs';
+import { authOptions } from "@/lib/auth.js";
+
+export async function GET() {
+    try {
+        const session = await getServerSession(authOptions);
+        // console.log(session);
+        if (!session || !session.user) {
+            return new Response(JSON.stringify({ error: "Login to register the events" }), { status: 401 });
+        }
+
+        const user = await prisma.user.findUnique({
+            where: { email: session.user.email }
+        });
+
+        if (!user) {
+            return new Response(JSON.stringify({ error: "User not found" }), { status: 404 });
+        }
+
+        const registeredEvents = await prisma.registration.findMany({
+            where: { userId: user.id },
+            select: { eventId: true }
+        });
+
+        return new Response(JSON.stringify(registeredEvents), { status: 200 });
+    } catch (error) {
+        console.error("Error fetching registered events:", error);
+        return new Response(JSON.stringify({ error: "Internal server error" }), { status: 500 });
+    }
+}
