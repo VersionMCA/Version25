61baa95b21ddfd3ee1f6746b9d57a9fd28de2325
Dainish Rahangdale <dainish74892002@gmail.com>
2024-12-30 20:51:23 +0530

    event page update

--- src/app/events/EventCard.jsx
+++ src/app/events/EventCard.jsx
@@ -7,72 +7,76 @@ import EventImage from "public/event_images/cryptic-min.jpg";
 
 const EventCard = ({ event, setShowEvent }) => {
   const [index, setIndex] = useState(0);
+  const [content, setContent] = useState(event.details[0].content);
 
   return (
-    <div className={` flex text-white justify-center items-center`}>
-      <div className={`flex flex-col w-full h-auto`}>
+    <div
+      style={{
+        backgroundImage: "url('/event_images/EventCardBg.jpg')",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+      }}
+      className={` flex text-white justify-center items-center -mt-28 h-screen relative `}
+    >
+      <div className={`w-full h-full mt-48  `}>
         {/* Header Section */}
-        <div className={`flex flex-col items-center w-1/3 p-7`}>
-          <h1 className="font-ROG text-xs lg:text-lg">{event.name}</h1>
+        <div className={`ml-16 p-7`}>
           <button onClick={() => setShowEvent(false)}>BACK</button>
-          <Image
-            src={EventImage}
-            height={200}
-            width={300}
-            alt="Event"
-            className={`rounded-xl`}
-          />
         </div>
+        <h1 className="font-ROG text-4xl lg:text-lg -mt-8 text-center">
+          {event.name}
+        </h1>
 
-        {/* Info Section */}
-        <div className={`flex justify-between w-full `}>
-          <div className="flex flex-col items-center w-1/2">
-            <p className="font-Orbitron text-sm">{event.date}</p>
-            <p className="font-Orbitron text-sm">{event.venue}</p>
-          </div>
-          <div className="flex flex-col items-center w-1/2">
-            <p className="font-Orbitron text-md font-black">{event.price}</p>
-            <p className="font-Orbitron text-sm">Prize Money</p>
-          </div>
-        </div>
-
-        {/* Details Section */}
-        <div className={`flex flex-col`}>
-          <div className={`flex flex-col w-full p-4 `}>
-            <div className="flex justify-around items-center w-full">
-              {event.details.map(({ title }, idx) => (
-                <div
-                  key={idx}
-                  className={`font-Orbitron text-xs cursor-pointer ${
-                    index === idx ? "border-b" : ""
-                  }`}
-                  onClick={() => setIndex(idx)}
-                >
-                  {title}
-                </div>
-              ))}
+        <div className="  w-3/4 h-1/2 mx-auto sm:mt-7 bg-sky-600 bg-opacity-85 rounded-2xl">
+          <div className="flex w-full h-5/6 ">
+            <div className="  w-1/5 my-auto p-3">
+              <Image
+                src={EventImage}
+                height={200}
+                width={200}
+                alt="Event"
+                className={`rounded-xl  mx-auto hidden md:block`}
+              />
             </div>
-            <div className={`w-full p-3`}>
-              <Markdown>
-                {event.details[index]?.content || "No content available"}
-              </Markdown>
+            <div className={`w-full pl-16 pt-16 overflow-hidden`}>
+              <Markdown>{content}</Markdown>
             </div>
           </div>
-
-          {/* Footer Section */}
-          <div className={`flex justify-center items-center h-1/6 `}>
-            <div className="flex items-center font-Orbitron text-sm">
-              <span>{event.date}</span>
-              <span className="ml-2 flex items-center">
+          <div className="w-full h-1/6 flex p-4 justify-between">
+            <div>
+              <p>{event.date}</p>
+            </div>
+            <div>
+              <p className="flex space-3">
+                {" "}
                 <Image src={MapDrop} alt="Venue" width={13} height={13} />
-                <span className="ml-2">{event.venue}</span>
-              </span>
+                {event.venue}
+              </p>
             </div>
-            <button>Register Now</button>
-            <div className="font-Orbitron text-sm">
-              PRIZE MONEY : {event.price}
+            <div>
+              <p>Prize Money {event.price}</p>
             </div>
           </div>
+        </div>
+
+        {/* tags Section */}
+
+        <div className="absolute bottom-0  -mb-7 md:mb-0 md:h-20 flex justify-around items-center w-full">
+          {event.details.map(({ title, content }, idx) => (
+            <div
+              key={idx}
+              className={`font-Orbitron text-xs cursor-pointer ${
+                index === idx ? "border-b-4 border-lime-400" : ""
+              }`}
+              onClick={() => {
+                setIndex(idx);
+                setContent(content ? content : "No content available");
+              }}
+            >
+              <h3 className="text-2xl">{title}</h3>
+            </div>
+          ))}
         </div>
       </div>
     </div>
