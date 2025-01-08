686987ba96519ce0b3bdea6a5f85b97992e50452
Vivek Kumar <142966563+109-Vivek@users.noreply.github.com>
2025-01-08 16:33:25 +0530

    Update UserDetailForm.jsx

--- src/app/profile/UserDetailForm.jsx
+++ src/app/profile/UserDetailForm.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import UserImage from "../../components/UserImage";
-import { Label } from "../../components/ui/Label";
-import { Input } from "../../components/ui/Input";
+import { Label } from "../../components/ui/label";
+import { Input } from "../../components/ui/input";
 import { Button } from "../../components/ui/Button";
 
 export default function UserDetailForm({ user }) {
