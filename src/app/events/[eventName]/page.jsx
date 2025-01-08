da3b510449fc62df15c1d27444547309f455a3a5
Vivek Kumar <vivekyadavnitt@gmail.com>
2025-01-08 17:54:16 +0530

    fix: build issues

--- src/app/events/[eventName]/page.jsx
+++ src/app/events/[eventName]/page.jsx
@@ -14,7 +14,8 @@ import toastStyle from "@/utilities/toastStyle";
 import { useSession } from "next-auth/react";
 import IndividualRegister from "./IndividualRegister";
 
-const BACKEND_URL = process.env.BACKEND_URL || "http://localhost:3000";
+const BACKEND_URL =
+  process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:3000";
 
 export default function page() {
   const session = useSession();
