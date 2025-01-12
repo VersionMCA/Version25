4b37bf18dc7abdeaf9f0bbd8001602fd1679eba7
Vivek Kumar <vivekyadavnitt@gmail.com>
2025-01-12 23:51:15 +0530

    minor fixes

--- src/app/events/EventThumbnail.jsx
+++ src/app/events/EventThumbnail.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { LazyLoadImage } from 'react-lazy-load-image-component';
-import './EventThumbnail.scss';
+import React from "react";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "./EventThumbnail.scss";
 
 function EventThumbnail({ index, imgLink, name, setNewItemActive }) {
-  const thumbnailClass = `thumbnail__item ${index === 0 ? 'active' : ''}`;
+  const thumbnailClass = `thumbnail__item ${index === 0 ? "active" : ""}`;
   return (
     <div
       className={thumbnailClass}
