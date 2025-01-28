6ecc9a7bba76bfc7ccfa730008960540465949f1
priyanshuwu <priyanshumiglani8@gmail.com>
2025-01-29 00:10:49 +0530

    updated footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
   return (
     <div
       id="footer"
-      className="absolute -bottom-24 bg-[#acda14cd] flex justify-center items-center p-1 w-screen text-sm md:text-md text-black orb font-semibold overflow-x-hidden"
+      className="absolute -bottom-24 bg-[#acda14cd] flex justify-center items-center p-1 w-screen text-xs sm:text-sm md:text-md text-black orb font-semibold overflow-x-hidden"
     >
       Made with{" "}
       <span id="heart" className="mx-2">
